ae6e3cd406fa05595bcd39ab08bd557160e7b41a
Merrick MacInnis <6237800@edu.vaniercollege.qc.ca>
2025-08-27 10:36:14 -0400

    first commit

---
+++ src/main/java/com/mycompany/zipcodeintro/ZipCodeIntro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.zipcodeintro;
+
+/**
+ *
+ * @author 6237800
+ */
+public class ZipCodeIntro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
